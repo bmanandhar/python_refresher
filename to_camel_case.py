eedce04450e33e2d5eb9f22b632a0930c447de38
Bijaya Manandhar <bijaya.manandhar@yahoo.com>
2019-11-03 15:59:32 -0800

    adds camelCase

---
+++ to_camel_case.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  3 15:58:21 2019
+
+@author: bijayamanandhar
+"""
+
+def to_camel_case(text):
+    #your code here
+    x = ""
+    
+    for char in text:
+        if char in "_-":
+            x = char
+    text = text.split(x)
+    new = [text[0]]
+    
+    for i in range(1, len(text)):
+        word = text[i].capitalize()
+        new.append(word)
+        
+    return ''.join(new)
+
+print(to_camel_case("the_stealth_warrior"))
+print(to_camel_case("The-Stealth-Warrior"))
