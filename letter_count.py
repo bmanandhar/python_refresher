ec3d2dc2a7918b2026c85643cf7872a80c321ff6
Bijaya Manandhar <bijaya.manandhar@yahoo.com>
2019-11-03 20:14:54 -0800

    adds letter_count

---
+++ letter_count.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  3 20:05:16 2019
+
+@author: bijayamanandhar
+"""
+
+# Write a function, `letter_count(str)` that takes a string and
+# returns a hash mapping each letter to its frequency. Do not include
+# spaces.
+#
+
+class Solution(object):
+    
+    def letter_count(self, str):
+        
+        dict = {}
+        
+        for char in str:
+            if char != ' ':
+                if char not in dict:
+                    dict[char] = 1
+                else:
+                    dict[char] += 1   
+                    
+        return dict
+    
+if __name__ == '__main__':
+    
+    strings = ['word', 'sentence', 'america', 'currency', 'fantastic']
+    S = Solution()
+    for i in range(len(strings)):
+        print(S.letter_count(strings[i]))
+    
+    
--- palindrome.py
+++ palindrome.py
@@ -25,4 +25,5 @@ if __name__ == '__main__':
         
         print(S.palindrome(string[i])) # Prints 'True'
         
-        
+    
+    
