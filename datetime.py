493d9fee634bc423674b8837fd7a3aa0aeb46b9a
Bijaya Manandhar <bijaya.manandhar@yahoo.com>
2019-11-04 09:02:49 -0800

    datetime workshop

---
+++ datetime.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  4 09:01:17 2019
+
+@author: bijayamanandhar
+"""
+
+#start time = x
+import time
+print(time.localtime())
+x = datetime.datetime.now()
+print('Start time: ', x)
+#code starts below
+    
+  
+    
+
+        
+#code ends here     
+#end time = y
+y = datetime.datetime.now()
+print('End time: ', y)
