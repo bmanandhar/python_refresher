f8dca9e04cc47b78239d6b5532f146040d5de1cc
Bijaya Manandhar <bijaya.manandhar@yahoo.com>
2019-11-04 22:38:55 -0800

    romanToInt

--- datetime.py
+++ datetime.py
@@ -5,18 +5,14 @@ Created on Mon Nov  4 09:01:17 2019
 
 @author: bijayamanandhar
 """
-
-#start time = x
+import datetime 
+#start time 
 import time
 print(time.localtime())
 x = datetime.datetime.now()
 print('Start time: ', x)
 #code starts below
-    
-  
-    
-
-        
+      
 #code ends here     
 #end time = y
 y = datetime.datetime.now()
@@ -26,3 +22,13 @@ print('End time: ', y)
 
 x = [1,2,3,6]
 print(x.index(2))
+
+x = { 'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000 }
+print(x['M'])
+
+
+
+
+
+
+
--- twoSum.py
+++ twoSum.py
@@ -54,6 +54,8 @@ if __name__ == '__main__':
         print(S.twoSum(example[0], example[1]))
     
     
-    
+x = { 'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000 }
+
+
     
     
