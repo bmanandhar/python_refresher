37e16d69214b61a1ef9626d142967fb903300f6f
Bijaya Manandhar <bijaya.manandhar@yahoo.com>
2019-11-04 20:23:12 -0800

    adds linear search

--- linear_search.py
+++ linear_search.py
@@ -37,7 +37,8 @@ if __name__ == "__main__":
         
         if S.linear_search(example[0], example[1]):
             print("Found at index:", i)
-        print("Not found")
+        else:
+            print("Not found")
         
         
     
